d37c5958d1260fe0007ca1ca75bcdf1c51eaee36
Umidjon Amirqulov <uamirqulov5@gmail.com>
2024-04-01 00:38:33 +0500

    add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,31 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation:{
+        fall: 'fall 1s linear'
+      },
+      animation:{
+        fall: 'fall 1.8s linear'
+      },
+      animation:{
+        bounce: 'bounce 1.2s 1.8s linear forwards'
+      },
+      keyframes: {
+        fall: {
+          '0%' : { transform: 'translateY(-100vh)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+        bounce: {
+          '0%, 40%, 75%, 95%' : { transform: 'translateY(0)', opacity:'1' },
+          '15%': { transform: 'translateY(-25px)' },
+          '65%': { transform: 'translateY(-15px)' },
+          '85%': { transform: 'translateY(-5px)' },
+          '100%': { transform: 'translateY(45px)',opacity:'1' },
+        }
+    },
+
   },
   plugins: [],
 }
-
+}
